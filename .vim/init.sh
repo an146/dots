0f0f5b49c11df91337f6c34f06a1a072005b4bbc
Anton Novikov <an146@ya.ru>
2011-08-03 17:23:33 +0400

    aliases, vim init

--- .vim/init.sh
+++ .vim/init.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+cd ~/.vim || exit 1
 rm -Rf bundle
 mkdir bundle
 git clone http://github.com/gmarik/vundle.git bundle/vundle
-vim '+BundleInstall!'
+vim '+BundleInstall!' '+q'
+[ -d bundle/Command-T ] && {
+    cd bundle/Command-T
+    rake make
+}
