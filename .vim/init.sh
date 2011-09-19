cf5257f35b3a44630b80694453dd50037aec3e40
Anton Novikov <an146@ya.ru>
2011-09-19 20:12:50 +0400

    really quitting vim

--- .vim/init.sh
+++ .vim/init.sh
@@ -3,7 +3,7 @@ cd ~/.vim || exit 1
 rm -Rf bundle
 mkdir bundle
 git clone http://github.com/an146/vundle.git bundle/vundle
-vim '+BundleInstall!' '+q'
+vim '+BundleInstall!' '+qa!'
 [ -d bundle/Command-T ] && {
     cd bundle/Command-T
     rake make
